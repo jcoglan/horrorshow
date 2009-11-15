5ad2729a4a4a86978f4f9fb221eb677a7a78cc62
James Coglan <jcoglan@googlemail.com>
2009-11-15 23:09:57 +0000

    Some parts of the grammar copied straight from the ECMAScript spec. Very little of it actually works.

--- lib/horrorshow.rb
+++ lib/horrorshow.rb
@@ -1,3 +1,16 @@
-class Horrorshow
-  VERSION = '1.0.0'
+require 'rubygems'
+require 'treetop'
+
+module Horrorshow
+  VERSION = '0.1.0'
 end
+
+%w[ whitespace
+    literals
+    comments
+    javascript
+
+].each do |grammar|
+  Treetop.load(File.dirname(__FILE__) + '/parser/' + grammar + '.tt')
+end
+
