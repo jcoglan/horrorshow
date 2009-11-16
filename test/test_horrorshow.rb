ba7b44435f6dc2f4980c683567744f80c64634b1
James Coglan <jcoglan@googlemail.com>
2009-11-16 08:38:25 +0000

    Begin a test suite. We can parse null, booleans and decimals.

--- test/test_horrorshow.rb
+++ test/test_horrorshow.rb
@@ -1,8 +1,19 @@
+$VERBOSE = nil
+
 require "test/unit"
 require "horrorshow"
 
 class TestHorrorshow < Test::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def self.parse_using(parser, *strings)
+    define_method("test #{parser}") do
+      parser = Horrorshow.const_get("#{parser}Parser").new
+      strings.each do |str|
+        assert_not_nil parser.parse(str), "attempting to parse #{str}"
+      end
+    end
   end
+  
+  parse_using :Literals,
+    "null", "true", "false",
+    "0", "4", "57", "8.4", "0.6", ".44", "9e+10", "4.5e-04", ".5E03"
 end
